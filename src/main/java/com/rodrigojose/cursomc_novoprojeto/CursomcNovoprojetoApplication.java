c19847689b88d1c4a1aef00b6e49344aefcc545f
Rodrigo Jose <rodrigo_jmauricio@outlook.com>
2021-02-17 22:14:44 -0300

    projeto criado

---
+++ src/main/java/com/rodrigojose/cursomc_novoprojeto/CursomcNovoprojetoApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigojose.cursomc_novoprojeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcNovoprojetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcNovoprojetoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodrigojose/cursomc_novoprojeto/CursomcNovoprojetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigojose.cursomc_novoprojeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcNovoprojetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
